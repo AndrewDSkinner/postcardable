1d2183a17ce5d212c8d01e25771cefc9614e8fb2
AndrewDSkinner <74809329+AndrewDSkinner@users.noreply.github.com>
2023-07-30 01:17:42 -0400

    initial project setup

---
+++ src/main/java/com/postcardable/postcardable/PostcardableApplication.java
@@ -0,0 +1,13 @@
+package com.postcardable.postcardable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostcardableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostcardableApplication.class, args);
+	}
+
+}
